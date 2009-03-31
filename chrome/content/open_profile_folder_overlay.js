69cf887ed1ec207743a2271210e7cb8b94d470d4
Kyo Nagashima <kyo@hail2u.net>
2009-04-01 01:30:10 +0900

    initial commit

---
+++ chrome/content/open_profile_folder_overlay.js
@@ -0,0 +1,15 @@
+var openProfileFolder = {
+  open: function() {
+    var directoryService = Components.classes['@mozilla.org/file/directory_service;1'].getService(Components.interfaces.nsIProperties);
+
+    var profileFolder = directoryService.get('ProfD', Components.interfaces.nsIFile);
+    this.runApplication(profileFolder.QueryInterface(Components.interfaces.nsILocalFile).path);
+  },
+
+  runApplication: function(aFolder) {
+    var fileLocal = Components.classes["@mozilla.org/file/local;1"].getService(Components.interfaces.nsILocalFile);
+
+    fileLocal.initWithPath(aFolder);
+    fileLocal.launch();
+  }
+}
