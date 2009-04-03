54f594dea3d4a6419b6abd79829f7c56e56201c9
Kyo Nagashima <kyo@hail2u.net>
2009-04-03 21:59:44 +0900

    add locale (en-US) add locale line and clean-up chrome.manifest stop supporting less than Fx3

--- content/overlay.js
+++ content/overlay.js
@@ -1,14 +1,12 @@
 var openProfileFolder = {
   open: function() {
     var directoryService = Components.classes['@mozilla.org/file/directory_service;1'].getService(Components.interfaces.nsIProperties);
-
     var profileFolder = directoryService.get('ProfD', Components.interfaces.nsIFile);
     this.runApplication(profileFolder.QueryInterface(Components.interfaces.nsILocalFile).path);
   },
 
   runApplication: function(aFolder) {
     var fileLocal = Components.classes["@mozilla.org/file/local;1"].getService(Components.interfaces.nsILocalFile);
-
     fileLocal.initWithPath(aFolder);
     fileLocal.launch();
   }
